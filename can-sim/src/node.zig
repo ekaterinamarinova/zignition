955dac49216de811ea58e0ea7f8437f396cae977
Marinova Ekaterina (BD/SWD-BEA4) <mae2sf@bosch.com>
2024-09-09 21:27:10 +0300

    send/recv remote frame

--- can-sim/src/node.zig
+++ can-sim/src/node.zig
@@ -1,23 +1,29 @@
 const std = @import("std");
 const can = @import("can-main.zig");
+const sr = @import("serial.zig");
 const net = std.net;
 const log = std.debug;
 const t = std.testing;
 
 var isFree = true;
 var i: usize = 0;
-var buffer: [30]u8 = undefined;
+var buffer: [60]u8 = undefined;
 
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const allocator = gpa.allocator();
 var boolBuff = std.ArrayList(bool).init(allocator);
+var result: *can.CanRemoteFrame = undefined;
 
 pub fn main() !void {
+    result = try allocator.create(can.CanRemoteFrame);
+    result.* = sr.createRemoteFrameEmpty();
+    std.debug.print("rf after creation: {any}\n", .{result.*});
     const t1 = try std.Thread.spawn(
         .{}, connect, .{"ecu", true});
     const t2  = try std.Thread.spawn(
         .{}, connect, .{"oxs", false});
 
+    defer allocator.destroy(result);
     t1.detach();
     t2.join();
 }
@@ -26,13 +32,22 @@ fn handler(client: net.Stream, doRead: bool) !void {
     if (doRead) {
         try read(client, "ecu");
     } else {
-        var rf = createRemoteFrame();
-        _ = &rf;
+        const rf = createRemoteFrame();
+        const serialized = try sr.serializeRemoteFrame(rf);
 
-        writeByte(client, rf.sof, "ecu");
-        std.time.sleep(10 * std.time.ns_per_s);
+        std.debug.print("Serialized frame: {any}\n", .{serialized.items});
+
+        for (serialized.items) |value| {
+            const cast: u8 = @intFromBool(value);
+            write(client, cast, "ecu");
+        }
+
+        std.time.sleep(20 * std.time.ns_per_s);
+
+        // now read the data frame
     }
 }
+
 
 pub fn connect(clName: []const u8, doRead: bool) !void {
     const address: []const u8 = "127.0.0.1";
@@ -41,47 +56,62 @@ pub fn connect(clName: []const u8, doRead: bool) !void {
     var client = try net.tcpConnectToAddress(add);
     log.print("Connected to server, from node: {s} handling...\n", .{clName});
 
-
     while (true) {
         try handler(client, doRead);
     }
 
+
     defer client.close();
 }
-
+var count: u32 = 0;
+//TODO enhance logic => detect sof, send bits for real time deserialization,
+//TODO send back ack and a data frame response
 fn read(stream: net.Stream, clName: []const u8) !void {
     var Mutex = std.Thread.Mutex{};
     Mutex.lock();
     defer Mutex.unlock();
 
+    var bit: bool = undefined;
     const byte = try stream.reader().readByte();
 
+    std.debug.print("\n [node] Full byte {b} count {d}\n", .{byte, count});
     switch (byte) {
         0 => {
-            log.print("Read dominant byte in node {s}.\n", .{clName});
-            try boolBuff.append(false);
+            bit = false;
         },
         1 => {
-            log.print("Read recessive byte in node {s}.\n", .{clName});
-            try boolBuff.append(true);
+            bit = true;
         },
         else => {
-            log.print("Error reading into buffer in node {s}.\n", .{clName});
+            log.print("[node] Error reading into buffer in node {s}.\n", .{clName});
+            return;
         },
     }
 
-    std.debug.print("Buffer: {any} \n", .{boolBuff.items});
+    var status: i8 = 0;
+
+    std.debug.print("rf object before: {any}\n", .{result});
+    status = try sr.mapBitsToFrames(bit, count, result);
+
+    if (status == 1) {
+        count = 0;
+        return;
+    }
+    
+    count += 1;
+    std.debug.print("result: {any}\n", .{result.*});
+
 }
 
-fn writeByte(stream: net.Stream, byte: u8, clName: []const u8) void {
+fn write(stream: net.Stream, bit: u8, clName: []const u8) void {
     var Mutex = std.Thread.Mutex{};
     Mutex.lock();
     defer Mutex.unlock();
 
-    log.print("Writing byte {b} from client {s}.. \n", .{byte, clName});
-    stream.writer().writeByte(byte) catch |err| {
+    // log.print("Writing byte {b} from client {s}.. \n", .{bit, clName});
+    stream.writer().writeByte(bit) catch |err| {
         if (error.WouldBlock == err) {
-            writeByte(stream, byte, clName);
+            write(stream, bit, clName);
         } else {
             log.print("Error writing to stream: {}\n", .{err});
         }
@@ -90,15 +120,14 @@ fn writeByte(stream: net.Stream, byte: u8, clName: []const u8) void {
 }
 
 fn createRemoteFrame() can.CanRemoteFrame {
-    const data = [_]u16{0, 0, 0, 0, 0, 0, 0, 0};
-    return can.CanRemoteFrame{
+    // const data = [_]u16{0, 0, 0, 0, 0, 0, 0, 0};
+    return can.CanRemoteFrame {
         .sof = 0b0,
-        //id(11b) + rtr(1b rec)
-        .arbitration = 0b010000001010,
-        .control = 0b00001000,
-        .crc = can.calculateCRC(&data),
-        .ack = 0b11,
-        .eof = 0b1111111
+        .arbitration = 0x01,
+        .control = 0x04,
+        .crc = 0x7FF3,
+        .ack = 0x1,
+        .eof = 0x7F,
     };
 }
 
