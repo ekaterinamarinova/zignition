435b8efcf68638f8e9af3fd3d6cfa2980107b8c2
Marinova Ekaterina (BD/SWD-BEA4) <mae2sf@bosch.com>
2024-09-18 01:20:24 +0300

    utility functions added, tests removed, data frame processing now works

--- zignition/src/serial.zig
+++ zignition/src/serial.zig
@@ -1,164 +1,33 @@
 const std = @import("std");
-const can = @import("can-main.zig");
+const bus = @import("bus.zig");
 const bits = @import("const/bits.zig").Bits;
 const log= std.log;
 const t = std.testing;
 
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const allocator = gpa.allocator();
-var rfp: *can.CanRemoteFrame = undefined;
-var dfp: *can.CanDataFrame = undefined;
+var rfp: ?*bus.CanRemoteFrame = null;
+var dfp: ?*bus.CanDataFrame = null;
 
 var isRemoteFrame = false;
 var isDataFrame = false;
 var identifier = std.ArrayList(bool).init(allocator);
+
+var dFieldLastBit: u8 = undefined;
+var dLen: u8 = 0;
+var bitBuff: u8 = 0;
+var bitCount: u8 = 0;
+var byteCount: u8 = 0;
+var dataPos: u8 = 0;
+
+var d = [_]u8{0,0};
 
 const ProcessingError = error {
     UnexpectedBit,
     ByteOverflow,
 };
 
-const Tag = enum {
-    CanDataFrame,
-    CanErrorFrame,
-    CanRemoteFrame,
-    CanInterframeSpacing
-};
-
-const CanUnion = union(Tag) {
-    CanDataFrame: can.CanDataFrame,
-    CanErrorFrame: can.CanErrorFrame,
-    CanRemoteFrame: can.CanRemoteFrame,
-    CanInterframeSpacing: can.CanInterframeSpacing
-};
-
-const Frames = struct {
-    tag: Tag,
-    value: CanUnion
-};
-
-
-test "serialize remote frame" {
-    const rf  = can.CanRemoteFrame {
-        .sof = 0b0,
-        .arbitration = 0x01,
-        .control = 0x02,
-        .crc = 0x7FF3,
-        .ack = 0x1,
-        .eof = 0x7F,
-    };
-
-    const bytes = try serializeRemoteFrame(rf);
-    try t.expectEqual(44, bytes.items.len);
-}
-
-test "deserialize remote frame" {
-    rfp = try allocator.create(can.CanRemoteFrame);
-    rfp.* = can.CanRemoteFrame {
-        .sof = 0b0,
-        .arbitration = 0x01,
-        .control = 0x04,
-        .crc = 0x7FF3,
-        .ack = 0x1,
-        .eof = 0x7F,
-    };
-    const rfs = try serializeRemoteFrame(rfp.*);
-    try t.expectEqual(44, rfs.items.len);
-    std.debug.print("{any}", .{rfs.items});
-
-    var count: u32 = 0;
-    var result: CanUnion = undefined;
-    for (rfs.items) |bit| {
-        result = try mapBitsToFrames(bit, count);
-        count += 1;
-    }
-
-    std.debug.print("\nExpected: {any}\n", .{rfp.*});
-    std.debug.print("result code: {any}\n", .{result});
-
-    switch (result) {
-        CanUnion.CanRemoteFrame => {
-            try t.expectEqual(0x01, rfp.arbitration);
-            try t.expectEqual(0x04, rfp.control);
-            try t.expectEqual(0x7FF3, rfp.crc);
-        },
-        else => {
-            std.debug.print("Unexpected result: {any}\n", .{result});
-        }
-    }
-
-}
-
-test "serialize data frame" {
-    var data = [_]u8{0b111};
-
-    const frame = can.CanDataFrame{
-        .sof = 0b0,
-        .arbitration = 0b000000000100,
-        .control = 0b000100,
-        .data = data[0..],
-        .crc = can.calculateCRC(&data),
-        .ack = 0b1,
-        .eof = 0x7F,
-    };
-
-    const bytes = try serializeDataFrame(frame);
-
-    //data lenght is 1 bit so rf bit size (44) + 1 = 45
-    try t.expectEqual(52, bytes.items.len);
-}
-
-var dFieldLastBit: u8 = undefined;
-test "deserialize data frame" {
-    dFieldLastBit = bits.DataFieldFirstBit.value();
-
-    var data = [_]u8{0b11111000, 0b10};
-    const frame = can.CanDataFrame{
-        .sof = 0b0,
-        .arbitration = 0b000000000100,
-        .control = 0b001000,
-        .data = &data,
-        .crc = can.calculateCRC(&data),
-        .ack = 0b1,
-        .eof = 0x7F,
-    };
-
-    var inData = [_]u8{0, 0};
-    dfp = try allocator.create(can.CanDataFrame);
-    dfp.* = can.CanDataFrame {
-        .sof = 0b0,
-        .arbitration = 0b0,
-        .control = 0b0,
-        .data = &inData,
-        .crc = 0,
-        .ack = 0,
-        .eof = 0,
-    };
-
-    std.debug.print("Frame vals: {any} \n", .{frame});
-    const bytes = try serializeDataFrame(frame);
-
-    try t.expectEqual(60, bytes.items.len);
-    std.debug.print("Serialized frame: {any}\n", .{bytes.items});
-
-
-    var count: u32 = 0;
-    for (bytes.items) |bit| {
-        // std.debug.print("Bit: {}, count: {d} \n", .{bit, count});
-        _ = try mapBitsToFrames(bit, count);
-        count += 1;
-    }
-
-    std.debug.print("Expected data frame: {any}\n", .{frame});
-    std.debug.print("Actual data frame: {any}\n", .{dfp.*});
-    try t.expectEqual(0b000000000100, dfp.arbitration);
-}
-
-pub fn allocateRemoteFrame() *can.CanRemoteFrame {
-    return try allocator.create(can.CanRemoteFrame);
-}
-
-pub fn mapBitsToFrames(bit: bool, order: u32) !CanUnion {
+pub fn mapBitsToFrames(bit: bool, order: u32) !bus.CanUnion {
     // if sof detected from caller
     switch (order) {
         0 => {
@@ -172,24 +41,38 @@ pub fn mapBitsToFrames(bit: bool, order: u32) !CanUnion {
             try identifier.append(bit);
             if (order == 12 and bit == true) {
                 // rtr bit recessive -> create remote frame
-                std.debug.print("is remote frame..\n", .{});
+                // std.debug.print("is remote frame..\n", .{});
                 isRemoteFrame = true;
             } else {
+                // std.debug.print("is data frame..\n", .{});
                 isDataFrame = true;
             }
         },
         13...108 => {
             if (isRemoteFrame) {
-                try deserializeRemoteFrame(bit, order, rfp);
+                if (rfp == null) {
+                    rfp = try allocator.create(bus.CanRemoteFrame);
+                    rfp.?.* = createRemoteFrameEmpty();
+                    identifier.clearRetainingCapacity();
+                }
+
+                try deserializeRemoteFrame(bit, order, rfp.?);
                 // last rf bit
                 if (43 == order) {
-                    std.debug.print("Setting is remote frame to false\n", .{});
                     isRemoteFrame = false;
-                    return CanUnion{ .CanRemoteFrame = rfp.* };
+                    return bus.CanUnion{ .CanRemoteFrame = rfp.? };
                 }
             } else {
-                try deserializeDataFrame(bit, order, dfp, identifier);
-                return CanUnion{ .CanDataFrame = dfp.* };
+                if (dfp == null) {
+                    dfp = try allocator.create(bus.CanDataFrame);
+                    dfp.?.* = createDataFrameEmpty();
+                }
+                if (identifier.items.len > 12) {
+                    identifier.clearRetainingCapacity();
+                }
+
+                try deserializeDataFrame(bit, order, &identifier);
+                return bus.CanUnion{ .CanDataFrame = dfp.? };
             }
         },
         else => {
@@ -198,10 +81,10 @@ pub fn mapBitsToFrames(bit: bool, order: u32) !CanUnion {
 
     }
 
-    return CanUnion{.CanErrorFrame = undefined};
+    return bus.CanUnion{.CanErrorFrame = undefined};
 }
 
-pub fn serializeDataFrame(frame: can.CanDataFrame) !std.ArrayList(bool) {
+pub fn serializeDataFrame(frame: bus.CanDataFrame) !std.ArrayList(bool) {
     var boolBuff = std.ArrayList(bool).init(allocator);
     // sof is always 1 dominant bit
     try boolBuff.append(false);
@@ -224,18 +107,14 @@ pub fn serializeDataFrame(frame: can.CanDataFrame) !std.ArrayList(bool) {
     }
 
     count2 = 0;
-    // std.debug.print("Full data is: {any} \n", .{frame.data});
 
     var byteC: u8 = 0;
     for (frame.data) |byte| {
-        // std.debug.print("Enter loop data item {b}\n", .{byte});
-        if (dLength == byteC) {
+        if (dLength < byteC) {
             break;
         }
-        // std.debug.print("Byte: {b}\n", .{byte});
         for (0..8) |_| {
             const bit: u8 = byte >> (7 - count2) & 1;
-            // std.debug.print(" Bit: {b} \n", .{bit});
             try append(bit, &boolBuff);
 
             if (count2 != 7) {
@@ -275,13 +154,13 @@ pub fn serializeDataFrame(frame: can.CanDataFrame) !std.ArrayList(bool) {
     return boolBuff;
 }
 
-var dLen: u8 = 0;
-var bitBuff: u8 = 0;
-var bitCount: u8 = 0;
-var byteCount: u8 = 0;
-var dataPos: u8 = 0;
-pub fn deserializeDataFrame(bit: bool, bitPosition: u32, df: *can.CanDataFrame, id: std.ArrayList(bool)) !void {
-    var f = df.*;
+pub fn deserializeDataFrame(bit: bool, bitPosition: u32, id: *std.ArrayList(bool)) !void {
+    // remote frame received, send back a data frame
+    var f = dfp.?.*;
+
+    if (bitPosition == 0) {
+        f.sof = 0;
+    }
 
     for (id.items) |b| {
         f.arbitration <<= 1;
@@ -309,21 +188,16 @@ pub fn deserializeDataFrame(bit: bool, bitPosition: u32, df: *can.CanDataFrame, 
         }
     }
 
-
     if (bitPosition > bits.ControlFieldLastBit.value() and
         bitPosition <= dFieldLastBit) {
 
         bitBuff = (bitBuff << 1) | @intFromBool(bit);
         bitCount += 1;
 
-        std.debug.print(" Bit val: {} Bit buff: {b} bit count: {d}\n", .{bit, bitBuff, bitCount});
 
-        if(bitCount == 8) {
-            std.debug.print("byte count: {d}, data: {any}, bitBuff {b}\n", .{byteCount, f.data, bitBuff});
-            // std.debug.print("bit count: {d} bit val {} bit position {d} dlen {d}\n", .{bitCount, bit, bitPosition, dLen});
+        if (bitCount == 8) {
             bitCount = 0;
-
-            f.data[byteCount] = bitBuff;
+            d[byteCount] = bitBuff;
             byteCount += 1;
             bitBuff = 0;
 
@@ -333,6 +207,8 @@ pub fn deserializeDataFrame(bit: bool, bitPosition: u32, df: *can.CanDataFrame, 
         }
     }
 
+    f.data = &d;
+
     if (bitPosition > dFieldLastBit and
         bitPosition <= (bits.CrcFieldLastBit.value() + dLen)) {
         f.crc <<= 1;
@@ -340,7 +216,6 @@ pub fn deserializeDataFrame(bit: bool, bitPosition: u32, df: *can.CanDataFrame, 
         if (bit) {
             f.crc |= 1;
         }
-
     }
 
     if (bitPosition > (bits.CrcFieldLastBit.value() + dLen) and
@@ -365,11 +240,10 @@ pub fn deserializeDataFrame(bit: bool, bitPosition: u32, df: *can.CanDataFrame, 
         }
     }
 
-    df.* = f;
+    dfp.?.* = f;
 }
 
-pub fn deserializeRemoteFrame(bit: bool, bitPosition: u32, rf: *can.CanRemoteFrame) !void {
-    log.debug("Bit position: {d}\n", .{bitPosition});
+pub fn deserializeRemoteFrame(bit: bool, bitPosition: u32, rf: *bus.CanRemoteFrame) !void {
     var f = rf.*;
     f.sof = 0;
     f.arbitration = 0x01;
@@ -426,7 +300,7 @@ pub fn deserializeRemoteFrame(bit: bool, bitPosition: u32, rf: *can.CanRemoteFra
    rf.* = f;
 }
 
-pub fn serializeRemoteFrame(frame: can.CanRemoteFrame) !std.ArrayList(bool) {
+pub fn serializeRemoteFrame(frame: bus.CanRemoteFrame) !std.ArrayList(bool) {
     var boolBuff = std.ArrayList(bool).init(allocator);
     // sof is always 1 dominant bit
     try boolBuff.append(false);
@@ -480,13 +354,60 @@ fn append(bit: u16, boolBuff: *std.ArrayList(bool)) !void {
     }
 }
 
-pub fn createRemoteFrameEmpty() can.CanRemoteFrame {
-    return can.CanRemoteFrame{
+pub fn createRemoteFrameEmpty() bus.CanRemoteFrame {
+    return bus.CanRemoteFrame{
         .sof = 0,
         .arbitration = 0x01,
         .control = 0,
         .crc = 0,
         .ack = 0,
         .eof = 0,
+    };
+}
+
+pub fn createDataFrameEmpty() bus.CanDataFrame {
+    var data = [_]u8{0,0};
+    return bus.CanDataFrame{
+        .sof = 0,
+        .arbitration = 0,
+        .control = 0,
+        .data = &data,
+        .crc = 0,
+        .ack = 0,
+        .eof = 0,
+    };
+}
+
+pub fn createRemoteFrame() bus.CanRemoteFrame {
+    return bus.CanRemoteFrame {
+        .sof = 0b0,
+        .arbitration = 0x01,
+        .control = 0x04,
+        .crc = 0x7FF3,
+        .ack = 0x1,
+        .eof = 0x7F,
+    };
+}
+
+pub fn createDataFrame() bus.CanDataFrame {
+    var data = [_]u8{0b11111000, 0b10};
+    return bus.CanDataFrame{
+        .sof = 0b0,
+        .arbitration = 0b000000000100,
+        .control = 0b001000,
+        .data = &data,
+        // .crc = can.calculateCRC(&data),
+        .crc = 0x0,
+        .ack = 0b1,
+        .eof = 0x7F,
+    };
+}
+
+pub fn createInterframeSpace() bus.CanInterframeSpacing {
+    return bus.CanInterframeSpacing{
+        .intermission = 0b111,
+        .suspendTransmission = 0b11111111,
+        // bus idle is of arbitrary length
+        .busIdle = 0b1111,
     };
 }
